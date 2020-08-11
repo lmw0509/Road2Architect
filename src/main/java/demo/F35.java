6da665587bbb315c9617f8a7f27de429af129eb5
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-08-11 09:33:23 +0800

    Update F35.java

--- src/main/java/demo/F35.java
+++ src/main/java/demo/F35.java
@@ -1,1 +1,15 @@
+import org.junit.Test;
 
+public class F35 {
+    public static final int i = 2;
+
+    public F35() {
+        // 该方法不会调用
+        System.out.println("调用构造函数");
+    }
+
+    @Test
+    public void main() {
+        System.out.println(F35.i);
+    }
+}
