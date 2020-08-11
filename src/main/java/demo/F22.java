72fe97be19eeaf5070d6d101355207076bcf7f44
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2020-08-11 09:32:48 +0800

    Update F22.java

--- src/main/java/demo/F22.java
+++ src/main/java/demo/F22.java
@@ -1,1 +1,12 @@
+class F22 {
+    public static final int i = 2;
 
+    F22() {
+        // 该方法不会调用
+        System.out.println("调用构造函数");
+    }
+
+    public static void main(final String[] args) {
+        System.out.println(F22.i);
+    }
+}
